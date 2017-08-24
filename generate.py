5496c25ef0faa271ff2385c408c286c83b1f484e
Thomas Leese <thomas@leese.io>
2017-08-24 08:53:12 +0100

    Add date parsing

--- generate.py
+++ generate.py
@@ -2,9 +2,13 @@
 
 from argparse import ArgumentParser
 
+import dateutil.parser
+
 
 def main():
     parser = ArgumentParser()
+    parser.add_argument('date', type=dateutil.parser.parse)
+    parser.add_argument('-o', '--output', default='tattoo.svg')
     args = parser.parse_args()
 
     print(args)
