2f608e943450c9f3f6495ce6b5c1a265166b4cb9
Thomas Leese <thomas@leese.io>
2017-09-01 15:20:21 +0100

    Update colours

--- generate.py
+++ generate.py
@@ -24,6 +24,8 @@ class Body:
 
 class Planet(Body):
 
+    fill = 'white'
+
     def __init__(self, orbit, name, radius, body):
         super().__init__(orbit)
 
@@ -34,7 +36,7 @@ class Planet(Body):
     def planet_radius_for_drawing(self, drawing):
         return (self.radius ** (1 / 5)) * (drawing['width'] * 0.0024)
 
-    def draw(self, drawing, fill):
+    def draw(self, drawing):
         angle = self.body.hlong + 2.9939488041
 
         orbit_radius = self.orbit_radius_for_drawing(drawing)
@@ -48,20 +50,23 @@ class Planet(Body):
 
         radius = self.planet_radius_for_drawing(drawing)
 
-        circle = drawing.circle(center=position, r=radius, fill=fill)
+        circle = drawing.circle(center=position, r=radius, fill=self.fill)
         drawing.add(circle)
 
 
 class Orbit(Body):
 
-    def draw(self, drawing, foreground_fill):
+    stroke = 'black'
+    stroke_width = 1
+
+    def draw(self, drawing):
         radius = self.orbit_radius_for_drawing(drawing)
 
         centre = (drawing['width'] / 2, drawing['height'] / 2)
 
         circle = drawing.circle(
             center=centre, r=radius,
-            fill='none', stroke=foreground_fill, stroke_width=1
+            fill='none', stroke=self.stroke, stroke_width=self.stroke_width
         )
 
         drawing.add(circle)
@@ -69,25 +74,29 @@ class Orbit(Body):
 
 class Sun(Body):
 
+    fill = 'white'
+
     def __init__(self):
         super().__init__(0)
 
-    def draw(self, drawing, foreground_fill):
+    def draw(self, drawing):
         centre = (drawing['width'] / 2, drawing['height'] / 2)
         radius = drawing['width'] * 0.04
 
-        circle = drawing.circle(center=centre, r=radius, fill=foreground_fill)
+        circle = drawing.circle(center=centre, r=radius, fill=self.fill)
         drawing.add(circle)
 
 
 class ClockHand(Body):
+
+    stroke = 'rgb(240, 240, 240)'
 
     def __init__(self, orbit, proportion, width):
         super().__init__(orbit)
         self.angle = (proportion - 0.25) * 2 * math.pi
         self.stroke_width = width
 
-    def draw(self, drawing, foreground_fill):
+    def draw(self, drawing):
         centre = (drawing['width'] / 2, drawing['height'] / 2)
         radius = self.orbit_radius_for_drawing(drawing)
 
@@ -98,13 +107,16 @@ class ClockHand(Body):
 
         line = drawing.line(
             start=centre, end=end,
-            stroke=foreground_fill, stroke_width=self.stroke_width
+            stroke=self.stroke, stroke_width=self.stroke_width
         )
 
         drawing.add(line)
 
 
 class MonthMarker(Body):
+
+    stroke = 'black'
+    stroke_width = 3
 
     def __init__(self, year, month):
         super().__init__(3)
@@ -118,7 +130,7 @@ class MonthMarker(Body):
 
         self.angle = (proportion - 0.25) * 2 * math.pi
 
-    def draw(self, drawing, foreground_fill):
+    def draw(self, drawing):
         centre = (drawing['width'] / 2, drawing['height'] / 2)
         radius = self.orbit_radius_for_drawing(drawing)
 
@@ -134,7 +146,7 @@ class MonthMarker(Body):
 
         line = drawing.line(
             start=start, end=end,
-            stroke=foreground_fill, stroke_width=3
+            stroke=self.stroke, stroke_width=self.stroke_width
         )
 
         drawing.add(line)
@@ -142,11 +154,13 @@ class MonthMarker(Body):
 
 class DayOfWeekMarker(Body):
 
+    fill = ClockHand.stroke
+
     def __init__(self, orbit, angle):
         super().__init__(orbit)
         self.angle = angle
 
-    def draw(self, drawing, fill):
+    def draw(self, drawing):
         orbit_radius = self.orbit_radius_for_drawing(drawing)
 
         centre = (drawing['width'] / 2, drawing['height'] / 2)
@@ -156,9 +170,9 @@ class DayOfWeekMarker(Body):
             centre[1] + orbit_radius * math.sin(self.angle),
         )
 
-        radius = drawing['width'] * 0.005
+        radius = drawing['width'] * 0.008
 
-        circle = drawing.circle(center=position, r=radius, fill=fill)
+        circle = drawing.circle(center=position, r=radius, fill=self.fill)
         drawing.add(circle)
 
 
@@ -169,31 +183,30 @@ class Clock:
 
         minute_proportion = date.minute / 60
 
-        self.parts = set(
+        self.parts = (
+            [MonthMarker(date.year, month) for month in range(1, 13)] +
             [
                 ClockHand(6, (date.hour % 12) / 12, 3),
                 ClockHand(9, minute_proportion, 2),
             ] +
-            [MonthMarker(date.year, month) for month in range(1, 13)] +
             [DayOfWeekMarker(i + 1.5, (minute_proportion - 0.25) * 2 * math.pi) for i in range(weekday + 1)]
         )
 
-    def draw(self, drawing, foreground_fill):
+    def draw(self, drawing):
         for part in self.parts:
-            part.draw(drawing, foreground_fill)
+            part.draw(drawing)
 
 
 class SolarSystemTattoo:
 
-    background_fill = 'rgb(50, 50, 50)'
-    foreground_fill = 'white'
+    background_fill = 'rgb(80, 80, 80)'
 
     def __init__(self, date, filename, size):
         self.drawing = svgwrite.Drawing(filename, size=(size, size))
 
-        self.bodies = set(
-            [Sun()] +
+        self.bodies = (
             [Orbit(i + 1) for i in range(8)] +
+            [Clock(date)] +
             [
                 Planet(1, 'Mercury', 2439.7, ephem.Mercury(date)),
                 Planet(2, 'Venus', 6051.8, ephem.Venus(date)),
@@ -204,7 +217,7 @@ class SolarSystemTattoo:
                 Planet(7, 'Uranus', 25362, ephem.Uranus(date)),
                 Planet(8, 'Neptune', 24622, ephem.Neptune(date)),
             ] +
-            [Clock(date)]
+            [Sun()]
         )
 
     def draw(self):
@@ -221,7 +234,7 @@ class SolarSystemTattoo:
 
     def draw_bodies(self):
         for body in self.bodies:
-            body.draw(self.drawing, self.foreground_fill)
+            body.draw(self.drawing)
 
     def save(self):
         self.drawing.save()
